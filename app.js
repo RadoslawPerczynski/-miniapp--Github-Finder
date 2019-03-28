587fad1739c67f69d724b9eda374453759e33c70
Radoslaw Perczynski <perczynski.radoslaw@gmail.com>
2019-03-28 22:28:48 +0100

    basic styles managed

---
+++ app.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById('searchUser');
+searchInput.addEventListener('input', () => {
+  if(this.value !== '') {
+    //console.log('fetching' + searchInput.value)
+  } else {
+    //showing error
+  }
+})
---
+++ github.js
@@ -0,0 +1,11 @@
+class GitHub {
+  constructor() {
+    this.client_id = '8271c5338d83d8405adb';
+    this.client_secret = '183ec6d2a95d10556765a76b57e2da40b75b1d9b';
+  }
+
+  getUserProfile(userName) {
+    //https://api.github.com/users/bradtraversy
+    //'https://api.github.com/users/whatever?client_id=xxxx&client_secret=yyyy'
+  }
+}
